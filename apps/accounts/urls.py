6d9ec10dd0a91611d67cb46391dfb9ea145a37ef
João Víctor Miranda Carvalho <joao.victor@rootssa.ba.gov.br>
2026-07-23 11:58:13 -0300

    feat/adicionando rota de token refresh

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from apps.accounts.views.register import RegisterView
 from apps.accounts.views.login import LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view(), name="login"),
-    #POST path("token/refresh/", TokenRefreshView.as_view()),
+    path(
+        "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     #GET path("me/", MeView.as_view()),
     #PATCH path("me/", MeView.as_view()),
     #POST path("change-password/", ChangePasswordView.as_view()),
